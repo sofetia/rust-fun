c617a9613ce5afd08ceca7ff429a16bfd6bb07ec
Sofia Cassandra Järg <sofiajarg@riseup.net>
2024-08-25 18:10:36 +0300

    started with slint calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+slint::include_modules!();
 mod cipher;
 use cipher::Cipher;
 use console::Term;
@@ -8,10 +9,12 @@ fn main() {
 
     println!("Select example to run:");
     println!("1. Caesar Cipher");
+    println!("2. slint window");
 
     if let Ok(character) = stdout.read_char() {
         match character {
             '1' => caesar_cipher(),
+            '2' => run_slint().expect("REASON"),
             _ => println!("Nothing selected"),
         }
     }
@@ -26,4 +29,18 @@ fn caesar_cipher() {
     let shift:u64 = 1;
     cipher.set_shift(shift);
     println!("Text out: {}", cipher.encrypt());
+}
+
+pub fn run_slint() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()
 }
